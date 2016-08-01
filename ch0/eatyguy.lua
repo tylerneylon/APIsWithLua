c2e50ffb7e7830f00407989406b7b8fea60f6a6f
Tyler Neylon <tylerneylon@gmail.com>
2016-07-31 20:26:03 -0700

    Some simplifications in eatyguy.lua.

--- ch0/eatyguy.lua
+++ ch0/eatyguy.lua
@@ -11,56 +11,45 @@ local grid_w, grid_h = nil, nil
 -- Internal functions.
 
 local function drill_from(x, y, already_visited)
-
-  -- 1. Set up supporting functions and data.
-  local function xy_str(x, y)
+  -- 1. Set up.
+  local function xy_str(x, y)       -- Useful for making table keys.
     return ('%d,%d'):format(x, y)
   end
-  if already_visited == nil then
-    -- This is a map ('x,y' -> true) for coordinates that we've already visited.
+  if already_visited == nil then    -- already_visited is nil for the root call.
     already_visited = {}
   end
+  grid[x][y] = true                     -- Mark x, y as an open space.
+  already_visited[xy_str(x, y)] = true  -- Mark that we've been here.
 
-  -- 2. Register this point as visited and not a wall.
-  if not grid[x][y] then
-    grid[x][y] = '.'
-  end
-  already_visited[xy_str(x, y)] = true
-
-  -- 3. Find available neighbors.
-  local nbors = {}
-  local deltas = {{-2, 0}, {2, 0}, {0, -2}, {0, 2}}
-  for _, d in pairs(deltas) do
-    local nx, ny = x + d[1], y + d[2]
+  -- 2. Find available neighbors.
+  local nbor_dirs = {}
+  for _, d in pairs({{-1, 0}, {1, 0}, {0, -1}, {0, 1}}) do
+    local nx, ny = x + 2 * d[1], y + 2 * d[2]
     if nx > 0 and nx <= grid_w and
        ny > 0 and ny <= grid_h and
        not already_visited[xy_str(nx, ny)] then
-        table.insert(nbors, {nx, ny})
+        table.insert(nbor_dirs, d)
     end
   end
 
-  -- 4. If we don't have nbors, we're at an endpoint in the depth-first srch.
-  if #nbors == 0 then return end
+  -- 3. If we have no nbor_dirs, we're at an endpoint in the depth-first srch.
+  if #nbor_dirs == 0 then return end
 
-  -- 5. If we have nbors, choose a random one and drill from there.
-  while #nbors > 0 do
-    local i      = math.random(1, #nbors)
-    local nx, ny = nbors[i][1], nbors[i][2]
-    local dx, dy = (nx - x) / 2, (ny - y) / 2
-    grid[x + dx][y + dy] = '.'  -- This will always be a wall until now.
-    drill_from(nx, ny, already_visited)
-    table.remove(nbors, i)
+  -- 4. If we have nbor_dirs, choose a random one and drill from there.
+  while #nbor_dirs > 0 do
+    local i = math.random(#nbor_dirs)
+    local d = nbor_dirs[i]
+    grid[x + d[1]][y + d[2]] = true  -- Set the adjacent grid cell as open.
+    drill_from(x + 2 * d[1], y + 2 * d[2], already_visited)
+    table.remove(nbor_dirs, i)
 
     -- Filter out visited nbors. This method lets some already visited nbors
     -- through the filter to add more paths to the maze.
-    local i = 1
-    while i <= #nbors do
+    for i = #nbor_dirs, 1, -1 do
       local is_extra_path_ok = (math.random(1, 100) <= percent_extra_paths)
-      local nx, ny = nbors[i][1], nbors[i][2]
+      local nx, ny = x + 2 * nbor_dirs[i][1], y + 2 * nbor_dirs[i][2]
       if already_visited[xy_str(nx, ny)] and not is_extra_path_ok then
-        table.remove(nbors, i)
-      else
-        i = i + 1
+        table.remove(nbor_dirs, i)
       end
     end
   end
@@ -82,6 +71,7 @@ function eatyguy.init()
   -- Draw the maze.
   for y = 0, grid_h + 1 do
     for x = 0, grid_w + 1 do
+      -- The next line is essentially: chars = (grid[x][y] ? '  ' : '##').
       local chars = (grid[x] and grid[x][y]) and '  ' or '##'
       io.write(chars)
     end
