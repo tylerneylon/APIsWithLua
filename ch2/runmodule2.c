0b7f929717cfa3c2e805bf07e142bcc2460a1285
Tyler Neylon <tylerneylon@gmail.com>
2016-08-05 04:21:35 -0700

    Chapter 2 checkpoint.

--- ch2/runmodule2.c
+++ ch2/runmodule2.c
@@ -55,21 +55,32 @@ int timestamp(lua_State *L) {
 
 int main() {
 
+  system("tput clear");  // Clear the screen.
+  system("tput civis");  // Hide the cursor.
+
   // Create a Lua state and load the module.
   lua_State *L = luaL_newstate();
   luaL_openlibs(L);
+
+  // Make our Lua-callable functions visible to Lua.
+  lua_register(L, "set_color", set_color);
+  lua_register(L, "set_pos",   set_pos);
+  lua_register(L, "timestamp", timestamp);
+
+  // Load eatyguy2 and run the init() function.
   luaL_dofile(L, "eatyguy2.lua");
   lua_setglobal(L, "eatyguy");
   lua_settop(L, 0);
-
-  // Make the set_color function visible to Lua.
-  lua_pushcfunction(L, set_color);
-  lua_setglobal(L, "set_color");
-
-  // Run the init() function.
   lua_getglobal(L, "eatyguy");
   lua_getfield(L, -1, "init");  // -1 means stack top.
   lua_call(L, 0, 0);            // 0, 0 = #args, #retvals
+
+  lua_getglobal(L, "eatyguy");
+  while (1) {
+    // Call eatyguy.loop().
+    lua_getfield(L, -1, "loop");
+    lua_call(L, 0, 0);
+  }
 
   return 0;
 }
