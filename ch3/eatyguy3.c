c7a724ebbda0d1ed7ffe06101a6e2f02b6658354
Tyler Neylon <tylerneylon@gmail.com>
2016-08-09 12:12:57 -0700

    ch3: add eatyguy3.{c,lua} to demo table usage.
    Next up I'm interested in looking at ways to reduce the size of main() in the C
    files.

---
+++ ch3/eatyguy3.c
@@ -0,0 +1,163 @@
+// eatyguy3.c
+//
+// Load eatyguy3.lua and run it in the order below.
+//
+// This introduces the use of Lua tables via Lua's C API.
+//
+//   -- Lua-ish pseudocode representing the order of events.
+//   eatyguy.init()
+//   while true do
+//     eatyguy.loop(tick)  -- tick has keys 'clock' and 'key'.
+//     sleep(0.016)
+//   end
+//
+//
+
+#include <fcntl.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include "lauxlib.h"
+#include "lua.h"
+#include "lualib.h"
+
+
+// Internal functions.
+
+double gettime() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec + 1e-6 * tv.tv_usec;
+}
+
+int getkey(int *is_end_of_seq) {
+
+  // We care about two cases:
+  // Case 1: A sequence of the form 27, 91, X; return X.
+  // Case 2: For any other sequence, return each int separately.
+
+  *is_end_of_seq = 0;
+  int ch = getchar();
+  if (ch == 27) {
+    int next = getchar();
+    if (next == 91) {
+      *is_end_of_seq = 1;
+      return getchar();
+    }
+    // If we get here, then we're not in a 27, 91, X sequence.
+    ungetc(next, stdin);
+  }
+  return ch;
+}
+
+void sleephires(double sec) {
+  long s = (long)floor(sec);
+  long n = (long)floor((sec - s) * 1e9);
+  struct timespec delay = { .tv_sec = s, .tv_nsec = n };
+  nanosleep(&delay, NULL);
+}
+
+void start() {
+
+  // Terminal setup.
+  system("tput clear");  // Clear the screen.
+  system("tput civis");  // Hide the cursor.
+  system("stty raw");    // Improve access to keypresses from stdin.
+
+  // Make reading from stdin non-blocking.
+  fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+}
+
+void done() {
+
+  // Put the terminal back into a decent state.
+  system("stty cooked");  // Undo earlier call to "stty raw".
+  system("tput reset");   // Reset terminal colors and clear the screen.
+
+  exit(0);
+}
+
+void push_tick_table(lua_State *L, int key, int is_end_of_seq) {
+
+  lua_newtable(L);
+
+    // stack = [.., {}]
+
+  if (is_end_of_seq && 65 <= key && key <= 68) {
+    // up, down, right, left = 65, 66, 67, 68
+    const char *arrow_names[] = {"up", "down", "right", "left"};
+    lua_pushstring(L, arrow_names[key - 65]);
+  } else {
+    lua_pushnumber(L, key);
+  }
+
+    // stack = [.., {}, key]
+
+  lua_setfield(L, -2, "key");
+
+    // stack = [.., {key = key}]
+
+  lua_pushnumber(L, gettime());
+
+    // stack = [.., {key = key}, clock]
+
+  lua_setfield(L, -2, "clock");
+
+    // stack = [.., {key = key, clock = clock}]
+}
+
+
+// Lua-visible functions.
+
+// Lua: timestamp().
+// Return a high-resolution timestamp in seconds.
+int timestamp(lua_State *L) {
+  lua_pushnumber(L, gettime());
+  return 1;
+}
+
+
+// Main.
+
+int main() {
+
+  start();
+
+  // Create a Lua state and load the module.
+  lua_State *L = luaL_newstate();
+  luaL_openlibs(L);
+
+  // Set up API functions written in C.
+  lua_register(L, "timestamp", timestamp);
+
+  // Set up API functions written in Lua.
+  luaL_dofile(L, "util.lua");
+
+  // Load eatyguy3 and run the init() function.
+  luaL_dofile(L, "eatyguy3.lua");
+  lua_setglobal(L, "eatyguy");
+  lua_settop(L, 0);
+
+  lua_getglobal(L, "eatyguy");
+  lua_getfield(L, -1, "init");  // -1 means stack top.
+  lua_call(L, 0, 0);            // 0, 0 = #args, #retvals
+
+  lua_getglobal(L, "eatyguy");
+  while (1) {
+    int is_end_of_seq;
+    int key = getkey(&is_end_of_seq);
+    if (key == 27 || key == 'q' || key == 'Q') done();
+
+    // Call eatyguy.loop(tick).
+    lua_getfield(L, -1, "loop");
+    push_tick_table(L, key, is_end_of_seq);
+    lua_call(L, 1, 0);
+
+    sleephires(0.016);  // Sleep for 16ms.
+  }
+
+  return 0;
+}
--- ch3/eatyguy3.lua
+++ ch3/eatyguy3.lua
@@ -1,4 +1,4 @@
--- eatyguy2.lua
+-- eatyguy3.lua
 
 local eatyguy = {}
 
@@ -54,7 +54,7 @@ end
 local move_delta     = 0.2  -- seconds
 local next_move_time = nil
 
-local function update(key)
+local function update(tick)
 
   -- Ensure any dot under the player has been eaten.
   local p = player.pos
@@ -63,14 +63,14 @@ local function update(key)
   -- Update the next direction if an arrow key was pressed.
   local dir_of_key = {left = {-1, 0}, right = {1, 0},
                       up   = {0, -1}, down  = {0, 1}}
-  local new_dir = dir_of_key[key]
+  local new_dir = dir_of_key[tick.key]
   if new_dir then player.next_dir = new_dir end
 
   -- Only move every move_delta seconds.
   if next_move_time == nil then
-    next_move_time = timestamp() + move_delta
+    next_move_time = tick.clock + move_delta
   end
-  if timestamp() < next_move_time then return end
+  if tick.clock < next_move_time then return end
   next_move_time = next_move_time + move_delta
 
   -- Change direction if we can; otherwise the next_dir will take effect if we
@@ -87,7 +87,7 @@ local function update(key)
   end
 end
 
-local function draw()
+local function draw(clock)
 
   -- Choose the sprite to draw.
   -- For example, a right-facing player is drawn as either '< or '-
@@ -99,7 +99,7 @@ local function draw()
   }
   local anim_timestep = 0.2
   local dirkey   = ('%d,%d'):format(player.dir[1], player.dir[2])
-  local framekey = math.floor(timestamp() / anim_timestep) % 2 + 1
+  local framekey = math.floor(clock / anim_timestep) % 2 + 1
   local chars    = draw_data[dirkey][framekey]
 
   -- Draw the player.
@@ -153,9 +153,9 @@ function eatyguy.init()
   end
 end
 
-function eatyguy.loop(key)
-  update(key)
-  draw()
+function eatyguy.loop(tick)
+  update(tick)
+  draw(tick.clock)
 end
 
 return eatyguy
