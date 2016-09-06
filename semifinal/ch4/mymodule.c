0e58886e9072114eeaee4552e61a7d9c0082ea2d
Tyler Neylon <tylerneylon@gmail.com>
2016-09-05 23:03:00 -0700

    ch4: Add mymodule.c.

---
+++ semifinal/ch4/mymodule.c
@@ -0,0 +1,11 @@
+
+// mymodule.c
+
+#include "lua.h"
+#include <stdio.h>
+
+int luaopen_mymodule(lua_State *L) {
+  printf("luaopen_mymodule() was called!\n");
+  lua_newtable(L);
+  return 1;  // Return the empty table.
+}
