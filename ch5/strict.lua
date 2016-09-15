77c3daf67eb5382ca69c02847d4473349ade3a1a
Tyler Neylon <tylerneylon@gmail.com>
2016-09-14 21:25:02 -0700

    ch5: Some clean up in strict.lua.

---
+++ ch5/strict.lua
@@ -0,0 +1,43 @@
+--[[ strict.lua
+
+Throw an error when a global is defined in a function or when an
+undeclared global is referenced in a function.
+
+There are a number of files similar to this one available
+online, as well as described in the Programming in Lua book.
+This is based on all of those influences.
+
+--]]
+
+local mt = getmetatable(_G)
+
+if mt == nil then
+  mt = {}
+  setmetatable(_G, mt)
+end
+
+mt.__declared = {}
+
+local function what ()
+  local d = debug.getinfo(3, 'S')
+  return d and d.what or 'C'
+end
+
+mt.__newindex = function (t, n, v)
+  if not mt.__declared[n] then
+    local w = what()
+    if w ~= 'main' and w ~= 'C' then
+      fmt = 'Attempt to assign to undeclared global "%s"'
+      error(fmt:format(n), 2)
+    end
+    mt.__declared[n] = true
+  end
+  rawset(t, n, v)
+end
+
+mt.__index = function (t, n)
+  if not mt.__declared[n] and what() ~= "C" then
+    error(('Use of undeclared global "%s"'):format(n), 2)
+  end
+  return rawget(t, n)
+end
