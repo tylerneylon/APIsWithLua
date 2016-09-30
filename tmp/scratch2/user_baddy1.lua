38dbe0cfece38a633186ce393e6a92ca03e950f4
Tyler Neylon <tylerneylon@gmail.com>
2016-09-29 22:22:37 -0700

    ch6: Write a good user script.

--- tmp/scratch2/user_baddy1.lua
+++ tmp/scratch2/user_baddy1.lua
@@ -16,30 +16,55 @@ end
 
 local dir = pair {1, 0}
 
+-- Return true iff either we can no longer move forward, or if
+-- a left or right turn is available.
+local function is_turning_point(possible_dirs)
+
+  local backwards       = pair {-dir.x, -dir.y}
+  local can_go_straight = false
+  local can_turn        = false
+  local straight_i      = 0
+
+  for i, possible_dir in pairs(possible_dirs) do
+    if possible_dir == dir then
+      can_go_straight = true
+      straight_i      = i
+    elseif possible_dir ~= backwards then
+      can_turn = true
+    end
+  end
+
+  return can_turn or not can_go_straight, straight_i
+end
+
+
 return function (baddy, possible_dirs, grid, player)
+
+  local is_turn_pt, straight_i = is_turning_point(possible_dirs)
+
+  if not is_turn_pt then
+    return straight_i
+  end
 
   local to_player = pair {player.pos[1] - baddy.pos[1],
                           player.pos[2] - baddy.pos[2]}
-  io.stderr:write('to_player = ' .. s(to_player) .. '\n')
-  local max_dot_prod = 0
-  local best_i       = 0
+
+  local max_dot_prod = -math.huge
+  local choice_i     = 0
 
   for i, possible_dir in pairs(possible_dirs) do
-    io.stderr:write('possible_dir = ' .. s(possible_dir) .. '\n')
     local dot_prod = dot(possible_dir, to_player)
-    io.stderr:write('dot_prod = ' .. dot_prod .. '\n')
     if dot_prod > max_dot_prod then
       max_dot_prod = dot_prod
-      best_i = i
-      io.stderr:write('best_i = ' .. best_i .. '\n')
+      choice_i = i
     end
-    --[[
-    if dir == possible_dir then
-      return i
-    end
-    --]]
   end
-  return best_i
+
+  if math.random(3) == 1 then
+    choice_i = math.random(#possible_dirs)
+  end
+  dir = possible_dirs[choice_i]
+  return choice_i
 
   --[[
   local i = math.random(#possible_dirs)
