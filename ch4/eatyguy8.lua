3251815da084cf035544e67e1212445476149a9c
Tyler Neylon <tylerneylon@gmail.com>
2016-10-30 11:06:25 -0700

    Consistentify maze-drawing Lua code.

--- ch4/eatyguy8.lua
+++ ch4/eatyguy8.lua
@@ -157,7 +157,8 @@ function eatyguy.init()
       end
       io.flush()                     -- Needed for color output.
     end
-    io.write('\r\n')                 -- Move cursor to next row.
+    set_color('b', 0)                -- End the lines in black.
+    io.write(' \r\n')                -- Move cursor to next row.
   end
 end
 
