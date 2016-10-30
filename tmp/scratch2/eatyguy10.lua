f0592a29e9f5d4dfd901ac3b4872e07500d3e74a
Tyler Neylon <tylerneylon@gmail.com>
2016-10-30 11:11:19 -0700

    Bring eatyguy10 up to speed.

--- tmp/scratch2/eatyguy10.lua
+++ tmp/scratch2/eatyguy10.lua
@@ -139,7 +139,7 @@ end
 function eatyguy.init()
 
   -- Set up the grid size and pseudorandom number generation.
-  grid_w, grid_h = 39, 23
+  grid_w, grid_h = 39, 21
   math.randomseed(os.time())
 
   -- Set up the baddies.
